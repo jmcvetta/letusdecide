01c74d9c986f4728655c129e4f9a6b6b8e2fc16a
Jason McVetta <jason.mcvetta@gmail.com>
2014-07-27 20:29:07 -0700

    connect past decision fragment

--- app/src/main/java/com/siliconheavy/letusdecide/MainActivity.java
+++ app/src/main/java/com/siliconheavy/letusdecide/MainActivity.java
@@ -89,7 +89,8 @@ public class MainActivity extends Activity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            // return PlaceholderFragment.newInstance(position + 1);
+            return PastDecisionFragment.newInstance("foo", "bar");
         }
 
         @Override
